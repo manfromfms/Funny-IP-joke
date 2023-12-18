010decd6e78577e8416df2ce8765985f9182cd59
normchell <cool.cool.alt1@gmail.com>
2023-12-18 23:05:33 +0500

    done for now

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
-const express = require('express');
-const { createCanvas } = require('canvas');
+const express = require("express");
+const {
+    createCanvas, Image
+} = require("canvas");
 const app = express();
 
-app.get('/', async (req, res) => {
-    res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    res.send(`<h1>This website is just a joke. No user data is stored</h1>`);
-})
+const fs = require('fs')
 
-app.get('/generate', async (req, res) => {
+app.get("/", async (req, res) => {
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.send(`<h1>This website is just a joke. No user data is stored</h1>`);
+});
+
+app.get("/generate", async (req, res) => {
     const canvas = createCanvas(800, 600);
-    const ctx = canvas.getContext('2d');
-    
-    const query = req.query;
-    
-    const clientIp = req.ip;
-    
-    ctx.font = '30px Arial';
-    
-    ctx.fillText(`IP: ${clientIp}`, 50, 50);
-    
-    const buffer = canvas.toBuffer('image/png');
-    
-    res.type('png');
+    const ctx = canvas.getContext("2d");
+
+    const clientIp = req.header("x-forwarded-for");
+
+    var responseIP = await fetch('https://freeipapi.com/api/json/' + clientIp)
+    var dataIp = await responseIP.json()
+
+    var text = ''
+
+    for (let i in dataIp) {
+        text += `${i}: ${dataIp[i]}\n`
+    }
+
+    ctx.font = "30px Arial";
+    ctx.fillText(`${text}`, 50, 50);
+
+    const buffer = canvas.toBuffer("image/png");
+
+    res.type("png");
     res.send(buffer);
 });
 
