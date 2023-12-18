1100b2aa1ad66cbe1f02c9ec628bfe73c3244277
normchell <cool.cool.alt1@gmail.com>
2023-12-18 22:14:14 +0500

    first code commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { createCanvas } = require('canvas');
+const app = express();
+
+app.get('/', async (req, res) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.send(`<h1>This website is just a joke. No user data is stored</h1>`);
+})
+
+app.get('/generate', async (req, res) => {
+    const canvas = createCanvas(800, 600);
+    const ctx = canvas.getContext('2d');
+    
+    const query = req.query;
+    
+    const ipAddress = req.connection.remoteAddress;
+    
+    ctx.font = '30px Arial';
+    
+    ctx.fillText(ipAddress, 50, 50);
+    
+    const buffer = canvas.toBuffer('image/png');
+    
+    res.type('png');
+    res.send(buffer);
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on :${port}`);
+});
