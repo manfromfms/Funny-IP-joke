903995a6c76796c191db76e259962afacd12c122
manfromfms <61939162+manfromfms@users.noreply.github.com>
2023-12-18 17:59:02 +0000

    finally working

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
-const express = require('express');
-const { createCanvas } = require('canvas');
+const express = require("express");
+const { createCanvas } = require("canvas");
 const app = express();
 
-app.get('/', async (req, res) => {
-    res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    res.send(`<h1>This website is just a joke. No user data is stored</h1>`);
-})
+app.get("/", async (req, res) => {
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  res.send(`<h1>This website is just a joke. No user data is stored</h1>`);
+});
 
-app.get('/generate', async (req, res) => {
-    const canvas = createCanvas(800, 600);
-    const ctx = canvas.getContext('2d');
-    
-    const query = req.query;
-    
-    const clientIp = req.ip;
-    
-    ctx.font = '30px Arial';
-    
-    ctx.fillText(`IP: ${clientIp}`, 50, 50);
-    
-    const buffer = canvas.toBuffer('image/png');
-    
-    res.type('png');
-    res.send(buffer);
+app.get("/generate", async (req, res) => {
+  const canvas = createCanvas(800, 600);
+  const ctx = canvas.getContext("2d");
+
+  const clientIp = req.header("x-forwarded-for");
+
+  ctx.font = "30px Arial";
+
+    var response = await fetch('https://freeipapi.com/api/json/' + clientIp)
+    var data = await response.json()
+
+    var text = ''
+
+    for(let i in data) {
+        text += `${i}: ${data[i]}\n`
+    }
+
+  ctx.fillText(`${text}`, 50, 50);
+
+  const buffer = canvas.toBuffer("image/png");
+
+  res.type("png");
+  res.send(buffer);
 });
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Server is running on :${port}`);
+  console.log(`Server is running on :${port}`);
 });
